0b7592f6d72af1065e917d7e8e6eeaf68c6f4adb
Mohammad Armaan <mohammadarmaan7786@Mohammads-MacBook-Air.local>
2026-04-17 19:58:24 +0530

    Pulling changes

--- Smart-Plot-Investment-Portal/backend/routes/adminRoutes.js
+++ Smart-Plot-Investment-Portal/backend/routes/adminRoutes.js
@@ -1,27 +1,34 @@
 const express = require("express");
-const router  = express.Router();
+const router = express.Router();
 const { authenticate, authorize, isAdmin } = require("../middleware/auth");
 const {
-  getPendingKYC, getAllBuilders, approveKYC, rejectKYC,
-  getPendingProjects, getAllProjects,
-  approveProject, rejectProject, activateProject, deactivateProject,
-  AllProjects,
+    getPendingKYC,
+    getAllBuilders,
+    approveKYC,
+    rejectKYC,
+    getPendingProjects,
+    getAllProjects,
+    approveProject,
+    rejectProject,
+    activateProject,
+    deactivateProject,
+    AllProjects,
 } = require("../controllers/adminController");
 
 router.use(authenticate, authorize("admin"));
 
 // KYC
-router.get("/kyc/pending",               getPendingKYC);
-router.get("/kyc/all",                   getAllBuilders);
-router.post("/kyc/:builderId/approve",   approveKYC);
-router.post("/kyc/:builderId/reject",    rejectKYC);
+router.get("/kyc/pending", getPendingKYC);
+router.get("/kyc/all", getAllBuilders);
+router.post("/kyc/:builderId/approve", approveKYC);
+router.post("/kyc/:builderId/reject", rejectKYC);
 
 // Projects
-router.get("admin/projects",authenticate,isAdmin,AllProjects)
-router.get("/projects/pending",                    getPendingProjects);
-router.get("/projects/all",                        getAllProjects);
-router.post("/projects/:projectId/approve",        approveProject);
-router.post("/projects/:projectId/reject",         rejectProject);
-router.post("/projects/:projectId/activate",       activateProject);
-router.post("/projects/:projectId/deactivate",     deactivateProject);
+router.get("admin/projects", authenticate, isAdmin, AllProjects);
+router.get("/projects/pending", getPendingProjects);
+router.get("/projects/all", getAllProjects);
+router.post("/projects/:projectId/approve", approveProject);
+router.post("/projects/:projectId/reject", rejectProject);
+router.post("/projects/:projectId/activate", activateProject);
+router.post("/projects/:projectId/deactivate", deactivateProject);
 module.exports = router;
